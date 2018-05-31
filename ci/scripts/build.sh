168a617a433d5a7ab8cb15cae1a6f7556c88e9b3
ricofehr <ricofehr@nextdeploy.io>
2018-05-31 09:00:37 +0200

    Ensure gradlew is present before build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -8,7 +8,7 @@ export TERM=${TERM:-dumb}
 
 for service in $MICROSLIST; do
     cd source-$service
-    ./gradlew --no-daemon clean assemble
+    [[ ! -f gradlew ]] || ./gradlew --no-daemon clean assemble
     cd ..
     cp source-${service}/build/libs/${service}-*-SNAPSHOT.jar  build-output/
 done
