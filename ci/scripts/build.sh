dce200292cd9de67c0aac52c891de959ae5469ce
ricofehr <ricofehr@nextdeploy.io>
2018-06-05 09:59:07 +0200

    Fix jar filename issue

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -10,7 +10,7 @@ for service in $MICROSLIST; do
     cd $service
     [[ ! -f gradlew ]] || ./gradlew --no-daemon clean assemble
     cd ..
-    cp ${service}/build/libs/${service}-*-SNAPSHOT.jar  build-output/
+    cp ${service}/build/libs/${service}-*.jar  build-output/
 done
 
 exit 0
