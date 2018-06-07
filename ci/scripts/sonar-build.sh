259d1deb0a4470df0c17e0e759d6309a9c8d5262
ricofehr <ricofehr@nextdeploy.io>
2018-06-07 09:54:39 +0200

    Add some sonar properties

--- ci/scripts/sonar-build.sh
+++ ci/scripts/sonar-build.sh
@@ -13,5 +13,7 @@ mkdir sonarqube-analysis-input/src
 mkdir sonarqube-analysis-input/classes
 [[ ! -d ${MICRONAME}/build/classes ]] || cp -rf ${MICRONAME}/build/classes/*  sonarqube-analysis-input/classes/
 cp -rf ${MICRONAME}/src/*  sonarqube-analysis-input/src/
+mkdir -p sonarqube-analysis-input/src/main
+mkdir -p sonarqube-analysis-input/src/test
 
 exit 0
