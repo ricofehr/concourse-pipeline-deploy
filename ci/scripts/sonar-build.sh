143691cdcd992d0f46e5fbda0fb17277f1f66d35
ricofehr <ricofehr@nextdeploy.io>
2018-06-08 08:52:07 +0200

    Add jacoco report path

--- ci/scripts/sonar-build.sh
+++ ci/scripts/sonar-build.sh
@@ -8,9 +8,14 @@ export TERM=${TERM:-dumb}
 
 cd $MICRONAME
 [[ ! -f gradlew ]] || ./gradlew --no-daemon clean assemble
+[[ ! -f gradlew ]] || ./gradlew --no-daemon test
+[[ ! -f gradlew ]] || ./gradlew --no-daemon jacocoTestReport
 cd ..
 mkdir sonarqube-analysis-input/src
 mkdir sonarqube-analysis-input/classes
+mkdir sonarqube-analysis-input/jacoco
+touch sonarqube-analysis-input/jacoco/test.exec
+[[ ! -d ${MICRONAME}/build/jacoco ]] || cp -rf ${MICRONAME}/build/jacoco/test.exec  sonarqube-analysis-input/jacoco/
 [[ ! -d ${MICRONAME}/build/classes ]] || cp -rf ${MICRONAME}/build/classes/*  sonarqube-analysis-input/classes/
 cp -rf ${MICRONAME}/src/*  sonarqube-analysis-input/src/
 mkdir -p sonarqube-analysis-input/src/main
